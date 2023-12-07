715c7ed0d9da40c8f0ccae44021ae49a11ef6a20
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-07 21:03:23 +0200

    feat: add basic folder structure and basic routes

---
+++ src/router/router.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home/Home';
+import { Layout } from '../components/Layout/Layout';
+import { Register } from '../pages/Register/Register';
+import { Login } from '../pages/Login/Login';
+
+export function Router(): ReactElement {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />}/>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </Routes>
+  );
+}
