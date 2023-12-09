2cf029a403a5319c7c0e7488cee8bc90922bf926
Alena <alenzija@gmail.com>
2023-12-09 12:57:26 +0100

    feat: implement TextField component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { TextField } from '../../components/inputs/TextFiled';
 
 export const Login: React.FC = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
+  return (
+    <div>
+      <TextField
+        id="email"
+        type="email"
+        label="Email:"
+        placeholder="name@example.com"
+        register={register}
+        error={errors['email']}
+      />
+    </div>
+  );
 };
