03ef4b7a5f5bd2f7eb805d7a19660dd1804e3752
Alena <alenzija@gmail.com>
2023-12-09 16:14:43 +0100

    chore: add firebase config

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -1,20 +1,21 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 export const loginSchema = yup.object().shape({
   email: yup
     .string()
     .required('this field is required')
     .email()
-    .matches(  /\.[a-z]{2,}$/,'email address must contain a domain').required(),
+    .matches(/\.[a-z]{2,}$/, 'email address must contain a domain')
+    .required(),
   password: yup
-  .string()  
-  .required('this field is required')
-  .min(8)
-  .matches(
-    /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+/,
-    'password must have a special character'
-  )
+    .string()
+    .required('this field is required')
+    .min(8)
+    .matches(
+      /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+/,
+      'password must have a special character'
+    )
     .matches(/[A-Z]+/, 'password must have one uppercased letter')
     .matches(/[a-z]+/, 'password must have one lowered letter')
-    .matches(/[0-9]+/, 'password must have one number')
+    .matches(/[0-9]+/, 'password must have one number'),
 });
