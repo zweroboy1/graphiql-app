3ae9dffa8ba970c8f86bd91506860adfc51524e8
Alena <alenzija@gmail.com>
2023-12-10 14:18:31 +0100

    feat: add unicode support

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -5,8 +5,7 @@ export const loginSchema = yup.object().shape({
     .string()
     .required('email is required')
     .email()
-    .matches(/\.[a-z]{2,}$/, 'email address must contain a domain')
-    .required(),
+    .matches(/\.[a-z]{2,}$/, 'email address must contain a domain'),
   password: yup
     .string()
     .required('password is required')
@@ -15,7 +14,7 @@ export const loginSchema = yup.object().shape({
       /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+/,
       'password must have a special character'
     )
-    .matches(/[A-Z]+/, 'password must have one uppercased letter')
-    .matches(/[a-z]+/, 'password must have one lowered letter')
+    .matches(/\p{Lu}/u, 'password must have one uppercased letter')
+    .matches(/\p{Ll}/u, 'password must have one lowered letter')
     .matches(/[0-9]+/, 'password must have one number'),
 });
