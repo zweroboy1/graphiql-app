7da722108615402aae029826d1a4272ad701e22a
zweroboy1 <fprognoz@gmail.com>
2023-12-10 18:49:53 +0200

    feat: install sass, create basic files for styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { App } from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
+import './assets/scss/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
