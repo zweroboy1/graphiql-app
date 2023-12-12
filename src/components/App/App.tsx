b50f14477cbf63aee3a78520d52834d73c2b99dc
Alena <alenzija@gmail.com>
2023-12-12 13:21:37 +0100

    fix: fix errors from redux

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,22 @@ import { Router } from '../../router/router';
 import { auth } from '../../firebase';
 import { useDispatch } from 'react-redux';
 
-import { updateUser } from '../../store/slices/useSlice';
+import { updateUser } from '../../store/slices/userSlice';
 
 export const App: React.FC = () => {
   const dispatch = useDispatch();
 
   auth.onAuthStateChanged((user) => {
-    dispatch(updateUser(user));
+    if (user) {
+      dispatch(
+        updateUser({
+          name: user.displayName as string,
+          email: user.email as string,
+        })
+      );
+    } else {
+      dispatch(updateUser(null));
+    }
   });
 
   return <Router />;
