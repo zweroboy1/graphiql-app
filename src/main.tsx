e71083ac1d4e599829eb9804079ca8ba3809ba85
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-12 18:50:02 +0200

    feat: add translation functionality to the onclick toggle button

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { App } from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import './assets/scss/style.scss';
+import { LanguageProvider } from './cantexts/locale.context';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <LanguageProvider>
       <Provider store={store}>
         <App />
       </Provider>
+      </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
