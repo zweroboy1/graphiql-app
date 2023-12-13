b41c0d32927b79fb3027035c09c9e84536842bb3
zweroboy1 <fprognoz@gmail.com>
2023-12-13 16:00:06 +0200

    feat(register): add registration form with fields validation

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
+import { loginUser } from '../../services/auth';
+import { TextField } from '../../components/inputs/TextField';
+import { PasswordField } from '../../components/inputs/PasswordField';
+import { registerSchema } from '../../schemas';
 
 export const Register: React.FC = () => {
-  return <div>Register</div>;
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm({ mode: 'onChange', resolver: yupResolver(registerSchema) });
+  const [submiting, setSubmiting] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit = async (data: { email: string; password: string }) => {
+    setSubmiting(true);
+    setError(null);
+    try {
+      await loginUser(data);
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      }
+    } finally {
+      setSubmiting(false);
+    }
+  };
+
+  return (
+    <>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="login-form__title h2">Sign up</h2>
+        <TextField
+          id="name"
+          type="text"
+          label="Name"
+          placeholder="Your name"
+          autocomplete="name"
+          register={register}
+          error={errors['name']}
+        />
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          placeholder="name@example.com"
+          autocomplete="username"
+          register={register}
+          error={errors['email']}
+        />
+        <PasswordField
+          id="password"
+          label="Password"
+          placeholder="Enter your password"
+          autocomplete="current-password"
+          register={register}
+          error={errors['password']}
+        />
+        <button
+          className="button"
+          disabled={!isValid || submiting}
+          type="submit"
+          role="submit"
+        >
+          Register
+        </button>
+        {false && error && <div>{error}</div>}
+
+        <div className="login-form__text text">
+          Already have an account?
+          <br />
+          <Link to="/login">Sign In</Link>
+        </div>
+      </form>
+    </>
+  );
 };
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,31 @@
-import { loginSchema } from './loginSchema';
+import * as yup from 'yup';
 
-export { loginSchema };
+const loginSchemaShape = {
+  email: yup
+    .string()
+    .required('email is required')
+    .email()
+    .matches(/\.[a-z]{2,}$/, 'email address must contain a domain'),
+  password: yup
+    .string()
+    .required('password is required')
+    .min(8)
+    .matches(
+      /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+/,
+      'password must have a special character'
+    )
+    .matches(/\p{Lu}/u, 'password must have one uppercased letter')
+    .matches(/\p{Ll}/u, 'password must have one lowered letter')
+    .matches(/[0-9]+/, 'password must have one number'),
+};
+
+const registerSchemaShape = {
+  name: yup
+    .string()
+    .required('name is required')
+    .matches(/^[\p{L}\s.-]+$/u, 'only letters are allowed'),
+  ...loginSchemaShape,
+};
+
+export const loginSchema = yup.object().shape(loginSchemaShape);
+export const registerSchema = yup.object().shape(registerSchemaShape);
