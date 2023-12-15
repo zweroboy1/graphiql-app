87d44971cbec0a5112e55d26c201b04e967fe2b3
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-15 02:25:22 +0200

    feat: add react transition group for modal and translate docs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,7 @@
 import { useLocalization } from '../../cantexts/locale.context';
 
 export const Home: React.FC = () => {
-  // const [schema, setSchema] = useState('');
-
-  // useEffect(() => {
-  //   fetchGraphQlSchema();
-  // }, [fetchGraphQlSchema]);
-
   const { translations } = useLocalization();
 
-  return (
-    <div>
-      {translations.Home}
-      {/* <div>{schema}</div> */}
-    </div>
-  );
+  return <div>{translations.Home}</div>;
 };
