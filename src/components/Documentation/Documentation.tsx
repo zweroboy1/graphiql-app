efc84732da0cec6f5a9b846af9e2e52d40d87319
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-15 02:25:22 +0200

    feat: add react transition group for modal and translate docs

--- src/components/Documentation/Documentation.tsx
+++ src/components/Documentation/Documentation.tsx
@@ -1,9 +1,12 @@
 import React, { useCallback, useState } from 'react';
 import '../Schema/Schema.css';
 import { SchemasArgs } from './SchemasArgs';
+import { useLocalization } from '../../cantexts/locale.context';
+import { Transition } from 'react-transition-group';
 
 export const Documentation: React.FC = () => {
   const [open, setOpen] = useState(false);
+  const { translations } = useLocalization();
   const toggleMenu = useCallback(async () => {
     setOpen(!open);
   }, [open]);
@@ -13,11 +16,15 @@ export const Documentation: React.FC = () => {
       <button className="btn" onClick={toggleMenu}>
         Docs
       </button>
-      {open && (
-        <div className={`menu ${open ? 'open' : ''}`}>
-          <SchemasArgs />
-        </div>
-      )}
+
+      <Transition in={open} timeout={300}>
+        {(state) => (
+          <div className={`menu ${state === 'entered' ? 'open' : ''}`}>
+            <div>{translations.Docs}</div>
+            <SchemasArgs />
+          </div>
+        )}
+      </Transition>
     </>
   );
 };
