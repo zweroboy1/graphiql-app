b1551cb91ef15061965da2893fc2bc4b8ad583b1
Alena <alenzija@gmail.com>
2023-12-18 19:25:02 +0100

    feat: implement QueryEditor with textarea

--- src/pages/GraphQl/GraphQl.tsx
+++ src/pages/GraphQl/GraphQl.tsx
@@ -1,11 +1,13 @@
 import { Documentation } from '../../components/Documentation/Documentation';
-import { QueryEditor } from '../../components/Editor/QueryEditor';
+// import { QueryEditor } from '../../components/Editor/QueryEditor';
+import { QueryEditorTextarea } from '../../components/Editor/QueryEditorTextarea';
 
 export const GraphQl: React.FC = () => {
   return (
     <>
       <Documentation />
-      <QueryEditor />
+      {/* <QueryEditor /> */}
+      <QueryEditorTextarea />
     </>
   );
 };
