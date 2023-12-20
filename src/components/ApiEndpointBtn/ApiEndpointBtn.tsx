0ff9c0267f95450c2b9cf1ac3107a1586917f7e9
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-20 22:34:51 +0200

    feat: add input for enter api url

--- src/components/ApiEndpointBtn/ApiEndpointBtn.tsx
+++ src/components/ApiEndpointBtn/ApiEndpointBtn.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setApiEndpointSlice } from '../../store/slices/apiEndpoint.slice';
 
-export const ApiEndpointBtn = () => {
-  return <div>ApiEndpointBtn</div>;
+export const ApiEndpointBtn: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const HandleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    dispatch(setApiEndpointSlice(inputValue));
+  };
+
+  return (
+    <div>
+      <input placeholder="Enter your api..." onChange={HandleInputChange} />
+    </div>
+  );
 };
