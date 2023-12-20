9ed1e9fcd4086b3641f036122199149bb35d3ebd
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-20 22:31:19 +0200

    feat: change api request

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,21 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getIntrospectionQuery } from 'graphql';
-import { apiUrl } from '../../utils/consts';
 import { RootObject } from '../../types/types';
 
 export const schemasApi = createApi({
   reducerPath: 'schemasApi',
-  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: '',
+  }),
   endpoints: (builder) => ({
-    getGraphQlSchema: builder.query<RootObject, void>({
-      query: () => ({
-        url: '',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query: getIntrospectionQuery() }),
-      }),
+    getGraphQlSchema: builder.query<RootObject, string>({
+      query: (url) => {
+        return {
+          url,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ query: getIntrospectionQuery() }),
+        };
+      },
     }),
   }),
 });
