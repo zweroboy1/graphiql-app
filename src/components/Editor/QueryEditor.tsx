02e6efce6dd58917f65ee3e77ba852bd0571aee2
zweroboy1 <fprognoz@gmail.com>
2023-12-26 16:28:47 +0200

    feat(playground): implement correct work of the execute, prettify, reset buttons

--- src/components/Editor/QueryEditor.tsx
+++ src/components/Editor/QueryEditor.tsx
@@ -1,76 +1,17 @@
-import {
-  /*ChangeEvent, useCallback, useEffect, useRef,*/ useState,
-} from 'react';
-
-import { /* useDispatch,*/ useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
-import { formatter } from '../../utils/queryEditor';
-
-/*
 import { setEditorValue } from '../../store/slices/editorSlice';
 import { setViewerValue } from '../../store/slices/viewerSlice';
-import { getTabCount } from '../../utils/queryEditor';
-*/
-
-import { useFetchGraphQlResponseMutation } from '../../store/api/api';
 import Editor from '@monaco-editor/react';
 
 type QueryEditorProps = {
   mode: 'editor' | 'viewer';
 };
 
-/*
-const api = useSelector((state: RootState) => state.apiEndpoint.api);
-
-const currentEditorValue = useSelector(
-  (state: RootState) => state.editor.value
-);
-const [prevEditorValue, setPrevEditorValue] = useState(currentEditorValue);
-const [fetchResponse, { isLoading }] = useFetchResponseMutation();
-const handleFetch = async () => {
-  try {
-    setPrevEditorValue(currentEditorValue);
-
-    const result = await fetchResponse({ query: currentEditorValue, url: api }).unwrap();
-
-    setValue(formatter(JSON.stringify(result)));
-  } catch (e) {
-    console.log(e);
-  }
-};
-*/
-
 export const QueryEditor: React.FC<QueryEditorProps> = ({ mode }) => {
-  // const [rowCounts, setRowCounts] = useState(1);
-  const [value, setValue] = useState('');
-  /*
- const [loading, setLoading] = useState(false);
-
   const dispatch = useDispatch();
-*/
-  const api = useSelector((state: RootState) => state.apiEndpoint.api);
-  // const currentEditorValue = useSelector((state: RootState) => state.editor.value);
-  // const [prevEditorValue, setPrevEditorValue] = useState(currentEditorValue);
-  const [fetchResponse /* , { isLoading } */] =
-    useFetchGraphQlResponseMutation();
 
-  const handleFetch = async (inputValue: string) => {
-    try {
-      // setPrevEditorValue(currentEditorValue);
-
-      console.log(inputValue, 'hhh');
-      const result = await fetchResponse({
-        query: inputValue /* currentEditorValue */,
-        url: api,
-      }).unwrap();
-      console.log(formatter(JSON.stringify(result)));
-      // setValue(formatter(JSON.stringify(result)));
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  /*
   const value = useSelector((state: RootState) =>
     mode === 'editor' ? state.editor.value : state.viewer.value
   );
@@ -83,98 +24,12 @@ export const QueryEditor: React.FC<QueryEditorProps> = ({ mode }) => {
     [mode, dispatch]
   );
 
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const caretRef = useRef<number | undefined>(undefined);
-
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const target = e?.target;
-    setValue(target.value);
-  };
-*/
-
   const handleChange = (inputValue: string | undefined) => {
-    if (inputValue) {
+    if (inputValue !== undefined) {
       setValue(inputValue);
-      handleFetch(inputValue);
     }
   };
 
-  /*
-  const onReset = () => {
-    setValue('');
-  };
-
-  const onFormat = () => {
-    if (value === '') {
-      return;
-    }
-    // setValue(formatter(value));
-    setValue((prevState) => formatter(prevState));
-  };
-
-  useEffect(() => {
-    if (textareaRef.current && caretRef.current) {
-      textareaRef.current.selectionStart = caretRef.current;
-      textareaRef.current.selectionEnd = caretRef.current;
-      caretRef.current = undefined;
-    }
-  }, [value]);
-
-  useEffect(() => {
-    const handleTab = (e: KeyboardEvent) => {
-      if (e.key === 'Tab') {
-        e.preventDefault();
-        const position = textareaRef.current?.selectionStart || value.length;
-
-        setValue(value.slice(0, position) + '\t' + value.slice(position));
-
-        caretRef.current = position + 1;
-      }
-    };
-    document.addEventListener('keydown', handleTab);
-    return () => {
-      document.removeEventListener('keydown', handleTab);
-    };
-  }, [setValue, value]);
-
-  useEffect(() => {
-    if (mode !== 'editor') return;
-
-    const handleEnter = (e: KeyboardEvent) => {
-      if (e.key === 'Enter') {
-        e.preventDefault();
-        const position = textareaRef.current?.selectionStart || value.length;
-        const tabCount = getTabCount(value.slice(0, position));
-        setValue(
-          value.slice(0, position) +
-            '\n' +
-            '\t'.repeat(tabCount) +
-            value.slice(position)
-        );
-        caretRef.current = position + tabCount + 1;
-      }
-    };
-    document.addEventListener('keydown', handleEnter);
-    return () => {
-      document.removeEventListener('keydown', handleEnter);
-    };
-  }, [value, setValue, mode]);
-
-  useEffect(() => {
-    setRowCounts([...value.matchAll(/\n/g)].length + 1);
-  }, [value]);
-
-  const handleFetch = async () => {
-    setPrevEditorValue(currentEditorValue);
-
-    const result = await fetchData({
-      query: currentEditorValue,
-      url: api,
-    }).unwrap();
-
-    setValue(formatter(JSON.stringify(result)));
-  };
-*/
   return (
     <div className="playground__monaco">
       <h4 className="h4 playground__title">
@@ -194,65 +49,11 @@ export const QueryEditor: React.FC<QueryEditorProps> = ({ mode }) => {
           hideCursorInOverviewRuler: true,
           overviewRulerLanes: 0,
           overviewRulerBorder: false,
-          //readOnly: true
+          readOnly: mode !== 'editor',
         }}
         onChange={handleChange}
         value={value}
       />
-      {/*
-        <textarea
-          ref={textareaRef}
-          value={value}
-          onChange={handleChange}
-          disabled={mode === 'viewer'}
-          style={{
-            width: '420px',
-            height: '520px',
-            tabSize: '10px',
-            fontFamily: 'inherit',
-          }}
-        />
-        
-        <div
-          style={{
-            position: 'absolute',
-            display: 'flex',
-            top: '5px',
-            left: '-20px',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          {Array.from({ length: rowCounts }, (_, i) => i + 1).map((item) => (
-            <div key={item}>{item}</div>
-          ))}
-        </div>*/}
     </div>
   );
 };
-
-/* mode === 'editor' ? (
-        <div>
-          <button type="button" onClick={onReset}>
-            Reset
-          </button>
-          <button type="button" onClick={onFormat}>
-            Format
-          </button>
-          <br />
-        </div>
-      ) : (
-        <div>
-          <button
-            disabled={
-              currentEditorValue === '' ||
-              currentEditorValue === prevEditorValue ||
-              loading
-            }
-            type="button"
-            onClick={handleFetch}
-          >
-            {loading ? 'loading....' : 'Run'}
-          </button>
-        </div>
-          ) */
--- src/pages/GraphQl/GraphQl.tsx
+++ src/pages/GraphQl/GraphQl.tsx
@@ -1,13 +1,65 @@
 import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 import { ApiEndpointBtn } from '../../components/ApiEndpointBtn/ApiEndpointBtn';
 import { Documentation } from '../../components/Documentation/Documentation';
 import { QueryEditor } from '../../components/Editor/QueryEditor';
+import { useFetchGraphQlResponseMutation } from '../../store/api/api';
+import { setEditorValue } from '../../store/slices/editorSlice';
+import { setViewerValue } from '../../store/slices/viewerSlice';
+import { formatter } from '../../utils/queryEditor';
 
 export const GraphQl: React.FC = () => {
   const [docsOpen, setDocsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const editorValue = useSelector((state: RootState) => state.editor.value);
+  const apiEndpoint = useSelector((state: RootState) => state.apiEndpoint.api);
+
+  const [fetchResponse /* , { isLoading } */] =
+    useFetchGraphQlResponseMutation();
+
   const toggleMenu = useCallback(async () => {
     setDocsOpen(!docsOpen);
   }, [docsOpen]);
+
+  const executeQuery = async () => {
+    if (docsOpen) {
+      toggleMenu();
+    }
+
+    await fetchResponse({
+      query: editorValue,
+      url: apiEndpoint,
+    })
+      .unwrap()
+      .then((payload) => {
+        // const jsonCode = formatter(JSON.stringify(payload)).trim();
+        const jsonCode = JSON.stringify(payload, null, 3).trim();
+        dispatch(setViewerValue(jsonCode));
+      })
+      .catch((error) => {
+        const jsonCode = JSON.stringify(error, null, 3).trim();
+        dispatch(setViewerValue(jsonCode));
+      });
+  };
+
+  const handleReset = () => {
+    dispatch(setEditorValue(''));
+    dispatch(setViewerValue(''));
+    if (docsOpen) {
+      toggleMenu();
+    }
+  };
+
+  const handlePrettify = () => {
+    if (!editorValue) {
+      return;
+    }
+    dispatch(setEditorValue(formatter(editorValue).trim()));
+    if (docsOpen) {
+      toggleMenu();
+    }
+  };
 
   return (
     <div className="playground">
@@ -25,15 +77,17 @@ export const GraphQl: React.FC = () => {
           <button
             className="button button_tool button_tool-reset"
             title="Reset query"
+            onClick={handleReset}
           ></button>
           <button
             className="button button_tool button_tool-format"
             title="Prettify query"
+            onClick={handlePrettify}
           ></button>
           <button
             className="button button_tool button_tool-play"
-            disabled
             title="Execute query"
+            onClick={executeQuery}
           ></button>
         </div>
         <div className="playground__viewer">
