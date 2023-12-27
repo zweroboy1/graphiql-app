8b0fad2dde55fa152bfde20c0b4214d71809cf9a
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-27 22:09:43 +0200

    fix: fix api endpoint component

--- src/components/ApiEndpointBtn/ApiEndpointBtn.tsx
+++ src/components/ApiEndpointBtn/ApiEndpointBtn.tsx
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { setApiEndpointSlice } from '../../store/slices/apiEndpoint.slice';
-import { RootState } from '../../store/store';
-import { useFetchGraphQlSchemaMutation } from '../../store/api/api';
 import { TextField } from '../../components/inputs/TextField';
 import { urlSchema } from '../../schemas';
-import { getErrorText } from '../../utils/getErrorText';
 import { clearAllHistory } from '../../store/slices/history.slice';
 import { setActiveType } from '../../store/slices/activeTypeSlice';
+import { RootState } from '../../store/store';
 
 export const ApiEndpointBtn: React.FC = () => {
   const {
@@ -18,45 +16,24 @@ export const ApiEndpointBtn: React.FC = () => {
     formState: { errors, isValid },
     handleSubmit,
   } = useForm({ mode: 'onChange', resolver: yupResolver(urlSchema) });
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-  const dispatch = useDispatch();
-
+  const [buttonDisabled] = useState(false);
   const apiUrl = useSelector((state: RootState) => state.apiEndpoint.api);
-  const [fetchSchema, { isLoading }] = useFetchGraphQlSchemaMutation();
+  const dispatch = useDispatch();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     if (!isValid) {
       return;
     }
-    const result = await fetchSchema(data.endpoint);
-    if ('error' in result) {
-      const errorCode =
-        'status' in result.error ? String(result.error.status) : null;
-      const toastText = getErrorText(errorCode, 'en');
-      setButtonDisabled(true);
-      toast.error(toastText, {
-        toastId: 'toast',
-        className: 'toast-error',
-      });
-      return;
-    }
+    dispatch(setApiEndpointSlice(data.endpoint));
 
     toast.success('Valid GraphQL endpoint!', {
       toastId: 'toast',
       className: 'toast-success',
     });
-    dispatch(setApiEndpointSlice(data.endpoint));
+
     dispatch(clearAllHistory());
     dispatch(setActiveType('Query'));
   };
-
-  useEffect(() => {
-    toast.onChange((v) => {
-      if (v.status === 'removed') {
-        setButtonDisabled(false);
-      }
-    });
-  }, []);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -73,8 +50,8 @@ export const ApiEndpointBtn: React.FC = () => {
         />
         <div className="playground__button-container">
           <button
-            className={isLoading ? 'button button_loading' : 'button'}
-            disabled={!isValid || isLoading || buttonDisabled}
+            className={'button'}
+            disabled={!isValid || buttonDisabled}
             type="submit"
             role="submit"
           >
