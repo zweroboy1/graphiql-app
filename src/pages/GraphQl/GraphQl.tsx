23ac3c0491dd24087db5a5716d522f32411459de
Angelina Liapin <angelina.liapin@gmail.com>
2023-12-27 12:51:47 +0200

    feat: add section for vars and headers

--- src/pages/GraphQl/GraphQl.tsx
+++ src/pages/GraphQl/GraphQl.tsx
@@ -8,6 +8,7 @@ import { useFetchGraphQlResponseMutation } from '../../store/api/api';
 import { setEditorValue } from '../../store/slices/editorSlice';
 import { setViewerValue } from '../../store/slices/viewerSlice';
 import { formatter } from '../../utils/queryEditor';
+import { VarsHeadersMenu } from '../../components/Editor/Vars&HeadersMenu';
 
 export const GraphQl: React.FC = () => {
   const [docsOpen, setDocsOpen] = useState(false);
@@ -67,6 +68,7 @@ export const GraphQl: React.FC = () => {
       <div className="playground__content">
         <div className="playground__editor">
           <QueryEditor key="editor" mode="editor" />
+          <VarsHeadersMenu />
         </div>
         <div className="playground__tools">
           <button
