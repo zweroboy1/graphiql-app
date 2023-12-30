3bedaa02129e9b77494bfc09b125981692ffecd4
zweroboy1 <fprognoz@gmail.com>
2023-12-30 21:51:31 +0200

    fix(header): add the debouncer to prevent scroll jumping problem
    n

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { logoutUser } from '../../services/auth';
@@ -13,12 +14,21 @@ export const Header: React.FC = () => {
   const user = useSelector((state: RootState) => state.user.value);
   const [scrollPos, setScrollPos] = useState(0);
   const [headerStyle, setHeaderStyle] = useState('');
+  const headerRef = useRef<HTMLDivElement>(null);
+
+  const debounce = (func: VoidFunction, delay: number) => {
+    let timer: NodeJS.Timeout;
+    return () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => func.apply(this), delay);
+    };
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
+    const handleScroll = debounce(() => {
       const position = window.scrollY;
       setScrollPos(position);
-    };
+    }, 100);
 
     window.addEventListener('scroll', handleScroll);
     return () => {
@@ -27,69 +37,78 @@ export const Header: React.FC = () => {
   }, []);
 
   useEffect(() => {
+    console.log('rerender');
+    let headerHeight = 0;
+    if (headerRef.current) {
+      headerHeight = headerRef.current.offsetHeight;
+    }
     setHeaderStyle(
-      scrollPos > COMPACT_HEADER_WHEN_SCROLL_Y ? ' header-compact' : ''
+      scrollPos - headerHeight > COMPACT_HEADER_WHEN_SCROLL_Y
+        ? 'header__wrapper-compact'
+        : ''
     );
   }, [scrollPos]);
 
   return (
-    <header className={`header${headerStyle}`}>
-      <div className="header__left">
-        <Link to="/" title="Welcome page">
-          <div className="header__logo-container">
-            <Logo className="header__logo-image" />
-            <span className="header__logo-text">GraphQL app</span>
-          </div>
-        </Link>
-        {user && (
-          <Link
-            className="button button_medium button_image button_image-playground"
-            to="/graph-ql"
-            title="GraphQL playground"
-          >
-            Playground
+    <header className={`header`}>
+      <div className={`header__wrapper ${headerStyle}`} ref={headerRef}>
+        <div className="header__left">
+          <Link to="/" title="Welcome page">
+            <div className="header__logo-container">
+              <Logo className="header__logo-image" />
+              <span className="header__logo-text">GraphQL app</span>
+            </div>
           </Link>
+          {user && (
+            <Link
+              className="button button_medium button_image button_image-playground"
+              to="/graph-ql"
+              title="GraphQL playground"
+            >
+              Playground
+            </Link>
+          )}
+        </div>
+        <LanguageButton />
+        {user ? (
+          <div className="header__right">
+            <div
+              className="header__user-container"
+              title={`Your Profile (${user.name})`}
+            >
+              <UserSvg className="header__user-image" />
+            </div>
+            <Link
+              className="button button_medium button_image button_image-logout"
+              to="/"
+              onClick={logoutUser}
+              title="Logout"
+              aria-label="Logout"
+            >
+              Logout
+            </Link>
+          </div>
+        ) : (
+          <div className="header__right">
+            <Link
+              className="button button_medium button_image button_image-login"
+              to="/login"
+              title="Login"
+              aria-label="Login"
+            >
+              Sign In
+            </Link>
+            <Link
+              className="button button_medium button_image button_image-register"
+              to="/register"
+              title="Register"
+              aria-label="Register"
+            >
+              Sign Up
+            </Link>
+          </div>
         )}
       </div>
-      <LanguageButton />
-      {user ? (
-        <div className="header__right">
-          <div
-            className="header__user-container"
-            title={`Your Profile (${user.name})`}
-          >
-            <UserSvg className="header__user-image" />
-          </div>
-          <Link
-            className="button button_medium button_image button_image-logout"
-            to="/"
-            onClick={logoutUser}
-            title="Logout"
-            aria-label="Logout"
-          >
-            Logout
-          </Link>
-        </div>
-      ) : (
-        <div className="header__right">
-          <Link
-            className="button button_medium button_image button_image-login"
-            to="/login"
-            title="Login"
-            aria-label="Login"
-          >
-            Sign In
-          </Link>
-          <Link
-            className="button button_medium button_image button_image-register"
-            to="/register"
-            title="Register"
-            aria-label="Register"
-          >
-            Sign Up
-          </Link>
-        </div>
-      )}
     </header>
   );
 };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,4 @@ export const LANGUAGES = [
   { id: 1, code: 'ru', label: 'Русский', abbr: 'Рус' },
 ];
 export const TOAST_DELAY = 3000;
-export const COMPACT_HEADER_WHEN_SCROLL_Y = 300;
+export const COMPACT_HEADER_WHEN_SCROLL_Y = 200;
