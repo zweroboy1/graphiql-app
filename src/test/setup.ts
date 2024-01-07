602849bcd5b45347f1dbf33368e16f588530e5c1
Alena <alenzija@gmail.com>
2024-01-07 20:17:07 +0100

    feat: add mock for firebase/auth

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+vi.mock('@firebase/auth', () => ({
+  getAuth: () => ({
+    onAuthStateChanged: (func: () => void) => {
+      func();
+      return () => {};
+    },
+  }),
+}));
